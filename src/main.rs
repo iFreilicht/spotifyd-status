f78c40a3d4f50cfbc1b44316f915963a6db4f053
Felix Uhl <felix.uhl@outlook.com>
2021-01-19 16:05:33 +0100

    Restructure everything

--- src/main.rs
+++ src/main.rs
@@ -22,65 +22,105 @@ const DELAY: Duration = Duration::from_millis(300);
 // Maximum number of characters in the output
 const MAX_WIDTH: usize = 20;
 
+fn is_spotifyd_running() -> bool {
+    Command::new("pgrep")
+        .arg("spotifyd")
+        .output()
+        .expect("Failed to execute pgrep!")
+        .status
+        .success()
+}
+
+/// Run playerctl to get info about currently playing track
+fn playerctl_output() -> Option<String> {
+    let output = Command::new("playerctl")
+        .args(&["--player=spotifyd", "metadata", "--format", FORMAT])
+        .output()
+        .expect("Failed to execute playerctl!");
+
+    // We can't process any further if we didn't get valid output
+    // This can happen if the track is paused or spotifyd didn't respond to the request
+    if output.status.code() != Some(0) {
+        return None;
+    }
+
+    Some(String::from_utf8_lossy(&output.stdout).into())
+}
+
+/// Format the buffer to contain the divider and a second copy of itself to ease scrolling
+fn format_buffer(playerctl_output: &str) -> String {
+    let mut buffer: String = playerctl_output.into();
+    buffer.pop(); // Remove trailing newline
+    buffer.push_str(DIVIDER);
+
+    // We need two copies back-to-back so the string slice seemlingly wraps around
+    buffer.push_str(&buffer.clone());
+    buffer
+}
+
+fn scroll_by(buffer: &str, scroll_amount: usize) -> &str {
+    // This may be zero, even if the buffer is 1 charactor long!
+    let half_length = buffer.len() / 2;
+
+    // Don't try to calculate anything else, it's not going to be possible
+    if half_length == 0 {
+        buffer
+    } else {
+        // TODO: This is not Unicode safe yet!
+        &buffer[scroll_amount..scroll_amount + min(MAX_WIDTH, half_length)]
+    }
+}
+
+fn advance_scroll_amount(buffer: &str, scroll_amount: usize) -> usize {
+    if buffer.len() <= MAX_WIDTH {
+        return 0;
+    }
+
+    // Move slice by one
+    (scroll_amount + 1) % (buffer.len() / 2)
+}
+
 fn main() {
-    let mut last_valid_output: Vec<u8> = vec![0];
+    let mut last_valid_output: String = String::new();
     let mut buffer: String = String::new();
     let mut scroll_amount: usize = 0;
     loop {
-        // Check whether spotify is even running first
-        let running = Command::new("pgrep")
-            .arg("spotifyd")
-            .output()
-            .expect("Failed to execute pgrep!")
-            .status
-            .success();
-
         // String slice that scrolls through the buffer
         let mut sliced: &str = "";
 
-        if !running {
+        // Check whether spotify is even running first
+        if !is_spotifyd_running() {
             // Clear buffers so we detect change properly when spotifyd comes back
             buffer.clear();
             last_valid_output.clear();
         } else {
-            // Get spotifyd data from playerctl
-            let playerctl = Command::new("playerctl")
-                .args(&["--player=spotifyd", "metadata", "--format", FORMAT])
-                .output()
-                .expect("Failed to execute playerctl!");
-
-            // Continue only if execution was successful. Otherwise, the previously received
+            // Process output only if playerctl ran successfully. Otherwise, the previously received
             // output is reused. This is useful as playerctl will randomly fail with spotifyd:
             // https://github.com/Spotifyd/spotifyd/issues/557
-            if playerctl.status.code() == Some(0) && last_valid_output != playerctl.stdout {
-                // Read changed output to buffer and format it
-                last_valid_output = playerctl.stdout;
-                buffer = String::from_utf8_lossy(&last_valid_output).into();
-                buffer.pop(); // Remove trailing newline
-                buffer.push_str(DIVIDER);
+            if let Some(output) = playerctl_output() {
+                if output != last_valid_output {
+                    // Read changed output to buffer and format it
+                    last_valid_output = output;
+                    buffer = format_buffer(&last_valid_output);
 
-                // We need two copies back-to-back so the string slice seemlingly wraps around
-                buffer.push_str(&buffer.clone());
-
-                // Reset slice position for new song
-                scroll_amount = 0
+                    // Reset slice position for new track
+                    scroll_amount = 0;
+                }
             }
 
-            // This may be zero, even if the buffer is 1 charactor long!
-            let half_length = buffer.len() / 2;
-
-            // Don't try to calculate anything else, it's not going to be possible
-            if half_length == 0 {
-                sliced = &buffer;
-            } else {
-                // Move slice by one
-                scroll_amount = (scroll_amount + 1) % half_length;
-                // TODO: This is not Unicode safe yet!
-                sliced = &buffer[scroll_amount..scroll_amount + min(MAX_WIDTH, half_length)];
-            }
+            sliced = scroll_by(&buffer, scroll_amount);
         }
 
+        // Polybar will re-draw a custom script on every new line it outputs
         println!("{}", sliced);
-        thread::sleep(DELAY);
+
+        if scroll_amount == 0 {
+            // If this is a new track, wait for a while before starting to scroll
+            // This makes the track easier to read initially
+            thread::sleep(DELAY * 10);
+        } else {
+            thread::sleep(DELAY);
+        }
+        scroll_amount = advance_scroll_amount(&buffer, scroll_amount);
     }
 }
